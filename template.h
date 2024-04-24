6f095b5d26c835a48296d5803d2addef7ded9454
Jason Wheatley <97367173+JasonWheatley@users.noreply.github.com>
2024-04-24 11:23:54 +0100

    Initial check in of template.h

---
+++ template.h
@@ -0,0 +1,13 @@
+/* ************************************************************************** */
+/**
+ * @file
+ * @author  J.L. Wheatley
+ * @brief   Functions for commanding Steeper Motor Driver
+ * @details TODO:
+ */
+/* ************************************************************************** */
+#ifndef MY_HEADER_H
+
+#define MY_HEADER_H
+
+#endif /* MY_HEADER_H */
